b88132fb04f593779cf06357b393c777fb85ba08
soul <soul.sxd@gmail.com>
2017-09-11 11:14:12 +0800

    add :+1:

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const globalShortcut = electron.globalShortcut
 var mainWindow = null;
 
 // 当所有窗口被关闭了，退出。
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
   // 在 OS X 上，通常用户在明确地按下 Cmd + Q 之前
   // 应用会保持活动状态
   if (process.platform != 'darwin') {
@@ -19,61 +19,82 @@ app.on('window-all-closed', function() {
 
 // 当 Electron 完成了初始化并且准备创建浏览器窗口的时候
 // 这个方法就被调用
-app.on('ready', function() {
+app.on('ready', function () {
 
-    globalShortcut.register('Ctrl+w',function(){
-        return;
-    })
+  globalShortcut.register('Ctrl+w', function () {
+    return;
+  })
 
   // 创建浏览器窗口。
   mainWindow = new BrowserWindow({
-        width: 1000, 
-        height: 600,
-        transparent: true, 
-        frame: false,
-        icon:__dirname + '/logo.png',
-    });
+    width: 1000,
+    height: 600,
+    transparent: true,
+    frame: false,
+    icon: __dirname + '/logo.png',
+    center: true
+  });
+  mainWindow.setHasShadow(true)
 
   // 加载应用的 index.html
   mainWindow.loadURL('https://im.dingtalk.com/');
-  
-  // 打开开发工具
-//   mainWindow.openDevTools();
 
-  mainWindow.on('resize', function(){
-      var winSize = mainWindow.getSize();
-      doResize(winSize[0], winSize[1]);
+  // 打开开发工具
+  // mainWindow.openDevTools();
+  mainWindow.setMenuBarVisibility(false)
+
+  mainWindow.on('resize', function () {
+    doResize();
   })
 
+  mainWindow.on('closed', function () {
   // 当 window 被关闭，这个事件会被发出
-  mainWindow.on('closed', function() {
     // 取消引用 window 对象，如果你的应用支持多窗口的话，
     // 通常会把多个 window 对象存放在一个数组里面，
     // 但这次不是。
     mainWindow = null;
   });
 
-  mainWindow.on('will-quit',function(){
-      globalShortcut.unregisterAll()
+  mainWindow.on('will-quit', function () {
+    globalShortcut.unregisterAll()
+  })
+
+
+  mainWindow.webContents.on('dom-ready', function(){
+    mainWindow.webContents.executeJavaScript(executeJavaScriptStr);    
+    doResize()
   })
 });
 
-
-
-var doResize = (x,y) => (
-    mainWindow.webContents.executeJavaScript(`
-    // layout-main 
-    // 1. flex 设置成当前高度 
-    // 2. width 100%
-    var layoutMain = document.getElementById('layout-main');
+var executeJavaScriptStr = `
+function doResize(x,y,fixPx){
+  console.log(arguments);
+  // layout-main 
+  // 1. flex 设置成当前高度 
+  // 2. width 100%
+  var layoutMain = document.getElementById('layout-main');
+  if(layoutMain){
     layoutMain.style.width = "100%";
-    layoutMain.style.flexBasis = "`+(y)+`px";
+    layoutMain.style.flexBasis = y+"px";
+  }
+  // layout-container 去掉 justify-content 属性
+  var layoutContainer = document.getElementById('layout-container');
+  if(layoutContainer){
+    layoutContainer.style.justifyContent = "inherit";
+  }
+  
+  // body 设置 成当前高度 - 59px
+  var body = document.getElementById('body');
+  if(body){
+    body.style.height = (y - fixPx) +"px";
+  }
+}
+`;
 
-    // layout-container 去掉 justify-content 属性
-    var layoutContainer = document.getElementById('layout-container');
-    layoutMain.style.justifyContent = "inherit";
-    // body 设置 成当前高度 - 59px
-    var body = document.getElementById('body');
-    body.style.height = "`+(y - 59)+`px";
-    `)
-)
+var doResize = () => {
+  let winSize = mainWindow.getSize();
+  let x = winSize[0];
+  let y = winSize[1];
+  let fixPx = 59;
+  mainWindow.webContents.executeJavaScript(`doResize(`+x+`,`+y+`,`+fixPx+`)`)
+};
